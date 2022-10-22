4b26ee315d6c2cdc1ece989110a86a3d4acd00dd
Saworieza <saworieza@gmail.com>
2022-10-22 14:48:37 +0300

    items on header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,10 +7,27 @@ const Hero = () => {
     <div className="hero">
       <div className='left-h'>
         <Header/>
+
+        {/* The best add  */}
         <div className='the-best-ad'>
           <div></div>
           <span>The best fitness club in town</span>
         </div>
+
+        {/* Hero Heading  */}
+        <div className='hero-text'>
+          <div>
+            <span className='stroke-text'>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>In here we will help you shape and build your ideal body and live up your life to the fullest</span>
+          </div>
+        </div>
+
       </div>
       <div className='right-h'>Right Side</div>
     </div>
