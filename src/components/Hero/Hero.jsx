ab706963d3c61b25eb226ac570a420f34a823c1e
Saworieza <saworieza@gmail.com>
2022-10-27 13:34:36 +0300

    hero left side complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,6 +27,28 @@ const Hero = () => {
             <span>In here we will help you shape and build your ideal body and live up your life to the fullest</span>
           </div>
         </div>
+        
+        {/* figures */}
+        <div className='figures'>
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <button className='btn'>Get Started</button>
+          <button className='btn'>Lean More</button>
+        </div>        
 
       </div>
       <div className='right-h'>Right Side</div>
